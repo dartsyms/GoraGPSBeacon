1fb1cc0d3e19ed10383b718eb35610b4570207a1
Alex Kh <dartsyms@gmail.com>
2017-11-05 04:48:20 +0400

    Remove google play services, rewrite location service

--- app/src/main/java/ru/kot/it/goragpsbeacon/services/UserLocationService.kt
+++ app/src/main/java/ru/kot/it/goragpsbeacon/services/UserLocationService.kt
@@ -1,140 +1,89 @@
 package ru.kot.it.goragpsbeacon.services
 
-import android.Manifest
 import android.app.Service
+import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
-import android.os.IBinder
-import android.provider.Settings
-import android.support.v4.app.ActivityCompat
 import android.util.Log
-import android.widget.Toast
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.api.GoogleApiClient
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
-import ru.kot.it.goragpsbeacon.constants.Constants
 
 
-class UserLocationService: Service(),
-        LocationListener,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+class UserLocationService: Service() {
 
-    private var mGoogleApiClient: GoogleApiClient? = null
-    private var mLocationRequest: LocationRequest? = null
-    private var callsCount: Int = 0
-    var locationIntent: Intent? = null
+    var locationManager: LocationManager? = null
 
-    override fun onCreate() {
-        super.onCreate()
-        Log.i("Location Service", "GoogleAPI call")
-        buildGoogleAPIClient()
-        locationIntent = Intent(Constants.CURRENT_LOCATION)
-    }
-
-    protected fun buildGoogleAPIClient() {
-        mGoogleApiClient = GoogleApiClient.Builder(this)
-                .addOnConnectionFailedListener(this)
-                .addConnectionCallbacks(this)
-                .addApi(LocationServices.API)
-                .build()
-    }
+    override fun onBind(intent: Intent?) = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.i("Location Service", "Check GoogleAPI Status")
-        callsCount = 0
-        when {
-            mGoogleApiClient == null -> buildGoogleAPIClient()
-            !mGoogleApiClient!!.isConnected -> {
-                mGoogleApiClient?.connect()
-                mLocationRequest = LocationRequest.create()
-                        .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                        .setInterval(Constants.LOCATION_INTERVAL)
-                        .setFastestInterval(5000)
-            }
-        }
+        super.onStartCommand(intent, flags, startId)
         return START_STICKY
     }
 
+    override fun onCreate() {
+        if (locationManager == null)
+            locationManager = applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
-    override fun onBind(intent: Intent?): IBinder? {
-        return null
-    }
-
-    override fun onConnected(bundle: Bundle?) {
-        when {
-            ActivityCompat.checkSelfPermission(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED -> return
-            ActivityCompat.checkSelfPermission(this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED -> return
+        try {
+            locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, INTERVAL, DISTANCE, locationListeners[1])
+        } catch (e: SecurityException) {
+            Log.e(TAG, "Fail to request location update", e)
+        } catch (e: IllegalArgumentException) {
+            Log.e(TAG, "Network provider does not exist", e)
         }
 
-        val location: Location? = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient)
-
-        when (location) {
-            null -> LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this)
-            else -> {
-                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this)
-                handleNewLocation(location)
-            }
+        try {
+            locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, INTERVAL, DISTANCE, locationListeners[0])
+        } catch (e: SecurityException) {
+            Log.e(TAG, "Fail to request location update", e)
+        } catch (e: IllegalArgumentException) {
+            Log.e(TAG, "GPS provider does not exist", e)
         }
     }
-
-
-    override fun onConnectionSuspended(p0: Int) {
-        Toast.makeText(this, "Disconnected. Please reconnect.", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onConnectionFailed(p0: ConnectionResult) {
-        Toast.makeText(this, "Connection failed.", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onLocationChanged(location: Location?) {
-        if (location != null) {
-            handleNewLocation(location)
-        }
-
-    }
-
 
     override fun onDestroy() {
         super.onDestroy()
-        stopLocationUpdate()
-    }
-
-    private fun handleNewLocation(location: Location) {
-        val mAccuracy: Float = location.accuracy
-        if (mAccuracy < Constants.GPS_ACCURACY_LEVEL) {
-            if (callsCount == 0) {
-                callsCount += 1
-                stopLocationUpdate()
+        if (locationManager != null)
+            for (i in 0..locationListeners.size) {
+                try {
+                    locationManager?.removeUpdates(locationListeners[i])
+                } catch (e: Exception) {
+                    Log.w(TAG, "Failed to remove location listeners")
+                }
             }
+    }
+
+
+    companion object {
+        val TAG = "LocationTrackingService"
+
+        val INTERVAL = 1000.toLong() // In milliseconds
+        val DISTANCE = 10.toFloat() // In meters
+
+        val locationListeners = arrayOf(
+                LTRLocationListener(LocationManager.GPS_PROVIDER),
+                LTRLocationListener(LocationManager.NETWORK_PROVIDER)
+        )
+
+        class LTRLocationListener(provider: String) : LocationListener {
+
+            val lastLocation = Location(provider)
+
+            override fun onLocationChanged(location: Location?) {
+                lastLocation.set(location)
+                // TODO: Send location to the server
+            }
+
+            override fun onProviderDisabled(provider: String?) {
+            }
+
+            override fun onProviderEnabled(provider: String?) {
+            }
+
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+            }
+
         }
-        locationIntent?.putExtra(Constants.INTENT_LOCATION_VALUE, location)
-        sendBroadcast(locationIntent)
-    }
-
-    private fun stopLocationUpdate() {
-        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this)
-        mGoogleApiClient?.disconnect()
-    }
-
-
-    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
-
-    }
-
-    override fun onProviderEnabled(provider: String?) {
-        Toast.makeText(getBaseContext(), "Gps is turned on!! ", Toast.LENGTH_SHORT).show();
-    }
-
-    override fun onProviderDisabled(provider: String?) {
-        val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-        startActivity(intent)
-        Toast.makeText(baseContext, "Gps is turned off!! ", Toast.LENGTH_SHORT).show()
     }
 }
