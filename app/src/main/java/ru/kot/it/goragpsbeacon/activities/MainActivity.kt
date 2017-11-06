d90782790a0d20c2586d24154a7abb7aea6ea24b
Alex Kh <dartsyms@gmail.com>
2017-11-06 06:38:45 +0400

    Start tracking click listener in MainActivity

--- app/src/main/java/ru/kot/it/goragpsbeacon/activities/MainActivity.kt
+++ app/src/main/java/ru/kot/it/goragpsbeacon/activities/MainActivity.kt
@@ -1,13 +1,38 @@
 package ru.kot.it.goragpsbeacon.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.kot.it.goragpsbeacon.R
+import ru.kot.it.goragpsbeacon.constants.Constants
+import ru.kot.it.goragpsbeacon.infrastructure.GoraGPSBeaconApp
+import ru.kot.it.goragpsbeacon.services.UserLocationService
+import ru.kot.it.goragpsbeacon.utils.PrefUtils
+import ru.kot.it.goragpsbeacon.utils.ServiceChecker
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        start_tracking.setOnClickListener { view ->
+            val isRegisteredUser: Boolean = PrefUtils.getBooleanFromPrefs(GoraGPSBeaconApp.getContext(),
+                    Constants.PREF_IS_LOGGED_IN_KEY, false)
+            val serviceIsRunning: Boolean = ServiceChecker.isServiceRunning(UserLocationService.javaClass, GoraGPSBeaconApp.getContext())
+
+            when (isRegisteredUser) {
+                true -> {
+                    val trackingIntent: Intent = Intent(this, UserLocationService::class.java)
+                    startService(trackingIntent)
+                }
+                false -> {
+                    val authIntent: Intent = Intent(this, LoginActivity::class.java)
+                    startActivity(authIntent)
+                }
+            }
+
+        }
     }
 }
